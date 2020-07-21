74c7f18ab61826f6f23a4e6034694f9f7d2c37c3
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-21 15:25:47 +0300

    feat: Add post image route

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const colors = require('colors');
 const errorHandler = require('./middleware/error');
 const fileUpload = require('express-fileupload');
 const ocrad = require('async-ocrad');
+const ErrorResponse = require('./utils/errorResponse')
 
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Change the environment to prodction
+if (process.argv[2] === '-p') {
+  process.env.NODE_ENV = 'prodction';
+}
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -16,6 +25,7 @@ const app = express();
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
+process.env.NODE_ENV = 'development';
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -44,7 +54,7 @@ app.set('view engine', 'ejs');
 
 // Routes
 
-app.get('', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.render('index');
 });
 
@@ -55,12 +65,34 @@ app.post('/', async (req, res, next) => {
       new ErrorResponse(`Please upload a file`, 400)
     );
   }
-  const file = req.files.file;
+  const image = req.files.image;
+
+  const text = await ocrad(image.data.toString());
+
+  // Move the photo to the public folder
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+    if (err) {
+      console.log(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
+
+    // Update the bootcamp photo name
+    const updatedBootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+    res.status(200).json({
+      success: true,
+      data: updatedBootcamp
+    });
+  });
+
+  console.log(text);
+
 
   // Check if the file is image
-  if (!file.mimetype.startsWith('image')) {
+  if (!image.mimetype.startsWith('image')) {
     return next(new ErrorResponse(`Please upload an image`, 400));
   }
+
 
 
 
