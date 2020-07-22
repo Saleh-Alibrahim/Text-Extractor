ea99cb8ad63c41d9d62ffea68ccb7a7689e246f3
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-22 06:12:03 +0300

    feat: Add sample image

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const getWorker = require('tesseract.js-node');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-// Change the environment to prodction
+// Change the environment to production
 if (process.argv[2] == '-p') {
-  process.env.NODE_ENV = 'prodction';
+  process.env.NODE_ENV = 'production';
 }
 
 // Load env vars
@@ -90,8 +90,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
-  PORT,
-  console.log(`Server running in on port ${PORT}`.yellow.bold
-  ));
+const server = app.listen(PORT, console.log(`Server running in on http://localhost:${PORT}`.yellow.bold));
 
